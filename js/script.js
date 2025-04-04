c0b61838c45102fc801b07dc371242a27a5e363f
luisespinosar93 <luis.espinosar93@gmail.com>
2025-04-04 13:13:26 -0600

    First commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    
+    //Click event to scroll to top
+    $('.diamante').click(function(){
+        $('html, body').animate({scrollTop : 0},800);
+        return false;
+    });
+
+});
